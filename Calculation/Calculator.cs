27b8ea2d747f5a7e7071da32f23edca45053c67e
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-06 19:52:54 +0530

    [Shivani] Added Calculation program

---
+++ Calculation/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+namespace Calculation
+{
+    internal class Calculator
+    {
+        public void AdditionofNumbers(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber + secondNumber;
+            Console.WriteLine("sum:"+result);
+        }
+        
+    }
+}
---
+++ Calculation/Program.cs
@@ -0,0 +1,20 @@
+﻿using Calculation;
+using System;
+
+namespace Testprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Calculator calculator = new Calculator(); ;
+            Console.WriteLine("Please enter 1st number");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter 2nd number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            calculator.AdditionofNumbers(firstNumber, secondNumber);
+            Console.ReadLine();
+        }
+    }
+}
